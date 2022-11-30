d93209b446a191e74c072cfb4836af192104286b
Goncalo-FradeIOHK <goncalo.frade@iohk.io>
2022-11-30 14:23:15 +0000

    feat(didcomm): add didcomm spm

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "DIDCommxSwift",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "DIDCommxSwift",
+            targets: ["DIDCommxSwift"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DIDCommxSwift",
+            dependencies: ["DIDCommx"],
+            path: "DIDCommxSwift/Sources/Swift"
+        ),
+        .target(
+            name: "DIDCommx",
+            dependencies: ["LibDidComm"],
+            path: "DIDCommxSwift/Sources/C"),
+        .binaryTarget(
+            name: "LibDidComm",
+            path: "./LibDidComm.xcframework.zip"
+        )
+    ]
+)
